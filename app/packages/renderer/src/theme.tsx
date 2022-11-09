04d2604c609e436f3992cbfe0a06df8af0e8eb67
Oliver Cooper <oliver@spalk.co>
2022-11-10 11:59:55 +1300

    Split theme helper

--- app/packages/renderer/src/theme.tsx
+++ app/packages/renderer/src/theme.tsx
@@ -1,26 +1,6 @@
 import { Global, css } from "@emotion/react";
 import { FunctionComponent } from "react";
-
-class ColourVariable {
-  name: string; // --var-name
-  public light: string;
-  public dark: string;
-  styles: string; // required for emotion to allow interpolation
-
-  constructor(name: string, light: string, dark: string) {
-    this.name = `--${name}`;
-    this.light = light;
-    this.dark = dark;
-    this.styles = `var(${this.name})`;
-  }
-
-  toString() {
-    return this.styles;
-  }
-}
-
-const colour = (name: string, light: string, dark: string) =>
-  new ColourVariable(name, light, dark);
+import { colour, darkLightStyles } from "./helpers/theme.helper";
 
 export const colours = {
   background: colour("background", "white", "black"),
@@ -109,23 +89,7 @@ const globalStyles = css`
   }
 `;
 
-// Helpers
-
-const darkStyles: string[] = [];
-const lightStyles: string[] = [];
-
-const addColours = (colours: Record<string, ColourVariable | unknown>) => {
-  for (const value of Object.values(colours)) {
-    if (value instanceof ColourVariable) {
-      darkStyles.push(`${value.name}: ${value.dark};`);
-      lightStyles.push(`${value.name}: ${value.light};`);
-    } else {
-      addColours(value as Record<string, ColourVariable>);
-    }
-  }
-};
-addColours(colours);
-
+const { darkStyles, lightStyles } = darkLightStyles(colours);
 export const ThemeStyles: FunctionComponent = () => (
   <Global
     styles={css`
